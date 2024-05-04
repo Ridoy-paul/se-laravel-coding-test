34f74e430704f8a25e7a782a87a791941a56f8c1
Ridoy Paul <cse.ridoypaul@gmail.com>
2024-05-05 02:43:41 +0600

    up

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Transactions;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -14,7 +17,24 @@ class HomeController extends Controller
     public function dashboard()
     {
         try {
-            return view('admin.pages.dashboard.home');
+            $userId = Auth::user()->id;
+            $currentMonth = Carbon::now()->format('Y-m');
+            $withdrawalsThisMonth = Transactions::where('user_id', $userId)->where('transaction_type', 'withdraw')
+                ->whereYear('date', Carbon::now()->year)
+                ->whereMonth('date', Carbon::now()->month)
+                ->get(['amount', 'fee']);
+            
+            $depositsThisMonth = Transactions::where('user_id', $userId)->where('transaction_type', 'deposit')
+                ->whereYear('date', Carbon::now()->year)
+                ->whereMonth('date', Carbon::now()->month)
+                ->sum('amount');
+
+            $totalWithdrawals = Transactions::where('user_id', $userId)->where('transaction_type', 'withdraw')->get(['amount', 'fee']);
+            $totalDeposits = Transactions::where('user_id', $userId)->where('transaction_type', 'deposit')->sum('amount');
+
+            $transactions = Transactions::where('user_id', $userId)->latest()->limit(20)->get();
+
+            return view('admin.pages.dashboard.home', compact('withdrawalsThisMonth', 'depositsThisMonth', 'totalWithdrawals', 'totalDeposits', 'transactions'));
         } 
         catch (\Exception $e) {
             return back()->withError($e->getMessage());
--- app/Utilities/TransactionUtility.php
+++ app/Utilities/TransactionUtility.php
@@ -30,11 +30,46 @@ class TransactionUtility
         return true;
     }
 
-    public static function totalWithdrawal($userId)
-    {
+    public static function totalWithdrawal($userId) {
         $totalWithdrawalAmount = Transactions::where('user_id', $userId)->where('transaction_type', 'withdrawal')->sum('amount');
         return $totalWithdrawalAmount;
     }
+
+    public static function checkThisMonth5kWithdrawl($userId) {
+        $currentMonthWithdrawals = Transactions::where('user_id', $userId)
+                                        ->where('transaction_type', 'withdraw')
+                                        ->whereYear('created_at', Carbon::now()->year)
+                                        ->whereMonth('created_at', Carbon::now()->month)
+                                        ->orderBy('id', 'ASC')
+                                        ->get(['id', 'amount']);
+            
+
+            $count = $currentMonthWithdrawals->count();
+            $totalWithdrawalAmountOfThisMonth = $currentMonthWithdrawals->sum('amount');
+            
+            if($totalWithdrawalAmountOfThisMonth >= 5000) {
+                $i = 1;
+                $calculatedAmount = 0;
+                foreach($currentMonthWithdrawals as $transactionItem) {
+                    $calculatedAmount += $transactionItem->amount;
+
+                    if($calculatedAmount >= 5000 && $i != $count) {
+                        $isFirst5KWithdrawalOfMonth = false;
+                        break;
+                    }
+                    else {
+                        $isFirst5KWithdrawalOfMonth = true;
+                    }
+
+                    $i++;
+                }
+            }
+            else {
+                $isFirst5KWithdrawalOfMonth = false;
+            }
+
+            return $isFirst5KWithdrawalOfMonth;
+    } 
 
     public static function calculateWithdrawalFee($userId, $amount)
     {
@@ -42,6 +77,7 @@ class TransactionUtility
         $individualAccountWithdrawalRate = 0.015;
         $businessAccountWithdrawalRate = 0.025;
         $withdrawalRate = 0;
+        $isTrueIndividualAccAndIsFirst1KWithdrawal = false;
 
         if (!$user) {
             return ['success' => false, 'message' => 'User not found!'];
@@ -49,20 +85,24 @@ class TransactionUtility
 
         if($user->account_type == 'individual') {
             $isFriday = Carbon::now()->dayOfWeek === Carbon::FRIDAY;
-            $isFirst1KWithdrawal = $amount <= 1000;
+            $isFirst1KWithdrawal = $amount > 1000;
 
-            $isFirst5KWithdrawalOfMonth = Transactions::where('user_id', $userId)
-                                        ->where('transaction_type', 'withdrawal')
-                                        ->whereMonth('created_at', Carbon::now()->month)
-                                        ->sum('amount') + $amount <= 5000;
+            $isFirst5KWithdrawalOfMonth = self::checkThisMonth5kWithdrawl($userId);
 
-            if ($isFriday || $isFirst1KWithdrawal || $isFirst5KWithdrawalOfMonth) {
+            if($isFriday) {
                 $withdrawalRate = 0;
-            } 
+            }
+            else if($isFirst1KWithdrawal && ($isFirst5KWithdrawalOfMonth <> true)) {
+                $withdrawalRate = $individualAccountWithdrawalRate;
+                $isTrueIndividualAccAndIsFirst1KWithdrawal = true;
+            }
+            else if($isFirst5KWithdrawalOfMonth) {
+                $withdrawalRate = 0;
+            }
             else {
                 $withdrawalRate = $individualAccountWithdrawalRate;
             }
-
+           
         }
         else if($user->account_type == 'business') {
             if (self::totalWithdrawal($userId) >= 50000) {
@@ -73,8 +113,11 @@ class TransactionUtility
             }
         }
 
-
+    
         $withdrawalFee = $amount * $withdrawalRate;
+        if($isTrueIndividualAccAndIsFirst1KWithdrawal) {
+            $withdrawalFee = ($amount - 1000) * $withdrawalRate;
+        }
         $totalAmount = ($amount + $withdrawalFee);
 
         if ($amount <= 0 || $totalAmount > $user->balance) {
